ad4bd07e7bcedd5c17ebddef38c379af4cf9748c
abhishek pandey <abhishekpy55@gmail.com>
2024-12-30 11:20:47 +0530

    Fixed a bug and added feature X

--- src/test/java/com/thetestingacadmey/ex_selenium/HelloWorld.java
+++ src/test/java/com/thetestingacadmey/ex_selenium/HelloWorld.java
@@ -4,5 +4,6 @@ package com.thetestingacadmey.ex_selenium;
 public class HelloWorld{
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("ABC");
     }
 }
