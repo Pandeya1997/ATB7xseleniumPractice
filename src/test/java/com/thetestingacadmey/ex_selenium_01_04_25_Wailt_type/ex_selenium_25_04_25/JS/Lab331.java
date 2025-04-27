caf74658f38197adbe86b1903a509e386f145818
abhishek pandey <abhishekpy55@gmail.com>
2025-04-27 23:21:37 +0530

    action &shawoldom

---
+++ src/test/java/com/thetestingacadmey/ex_selenium_01_04_25_Wailt_type/ex_selenium_25_04_25/JS/Lab331.java
@@ -0,0 +1,5 @@
+package com.thetestingacadmey.ex_selenium_01_04_25_Wailt_type.ex_selenium_25_04_25.JS;
+
+public class Lab331 {
+
+}
---
+++ src/test/java/com/thetestingacadmey/ex_selenium_24_04_25/ActionClass/Lab_332.java
@@ -0,0 +1,44 @@
+package com.thetestingacadmey.ex_selenium_24_04_25.ActionClass;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.*;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Lab_332 {
+    EdgeOptions options;
+    WebDriver driver;
+    @BeforeSuite
+    public void OpenBrowser(){
+      options =new EdgeOptions();
+      options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+      driver = new EdgeDriver(options);
+      driver.manage().window().maximize();
+    }
+    @Test(testName = "shadow & USE JavaScript")
+    @Description("Shadow Test And scrooling Managin broser throgh JS")
+    public void shadowTest(){
+        String URL = "https://awesomeqa.com/practice.html";
+        driver.get(URL);
+    // name = "firstname"
+        // THE TESTING ACADEMY
+        WebElement firstname = driver.findElement(By.name("firstname"));
+        Actions actions = new Actions(driver);
+        actions.keyDown(Keys.SHIFT).sendKeys(firstname,"thetestingacadmey").keyUp(Keys.SHIFT).build().perform();
+
+
+    }
+    @AfterSuite
+    public void ClosedBrowser(){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
