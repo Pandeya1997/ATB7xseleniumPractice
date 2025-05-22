7e07872bb232ff6df2895bd7ebb9ee9f229f00eb
abhishek pandey <abhishekpy55@gmail.com>
2025-05-22 17:29:31 +0530

    code data driven excel sheet

--- src/test/java/com/thetestingacadmey/Data_Driven_19_05_25_Data_Driven/RealTimeSenarios/Lab_352_DataDriven_Excel_reader.java
+++ src/test/java/com/thetestingacadmey/Data_Driven_19_05_25_Data_Driven/RealTimeSenarios/Lab_352_DataDriven_Excel_reader.java
@@ -1,6 +1,5 @@
 package com.thetestingacadmey.Data_Driven_19_05_25_Data_Driven.RealTimeSenarios;
 
-import io.qameta.allure.Description;
 import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
@@ -15,15 +14,16 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.time.Duration;
 
-public class Lab_351_DDTRealTIme {
+public class Lab_352_DataDriven_Excel_reader {
     EdgeOptions options;
     WebDriver driver;
-    WebDriverWait  wait;
+    WebDriverWait wait;
 
     @BeforeSuite
-    public void OpenBrowser(){
+    public void OpenBrowser() {
         options = new EdgeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new EdgeDriver(options);
@@ -32,49 +32,39 @@ public class Lab_351_DDTRealTIme {
     }
 
     @Test(dataProvider = "loginData")
-    public void dataDriven(String testid, String email, String password, String expectedResult) throws InterruptedException{
-     driver.get("https://app.vwo.com");
-     WebElement emailElement = driver.findElement(By.id("login-username"));
-   //  emailElement.clear();
-     emailElement.sendKeys(email);
-     WebElement PasswordElement = driver.findElement(By.id("login-password"));
- //    PasswordElement.clear();
-     PasswordElement.sendKeys(password);
+    public void testDataDriven(String email, String password, String expectedResult) {
+        driver.get("https://app.vwo.com");
+        WebElement emailElement = driver.findElement(By.id("login-username"));
+        emailElement.clear();
+        emailElement.sendKeys(email);
+        WebElement passwordElement = driver.findElement(By.id("login-password"));
+        passwordElement.clear();
+        passwordElement.sendKeys(password);
+        driver.findElement(By.id("js-login-btn")).click();
 
-     driver.findElement(By.id("js-login-btn")).click();
         if (expectedResult.equalsIgnoreCase("Valid")) {
-
-//            String text = driver.findElement(By.cssSelector("[data-qa=\"lufexuloga\"]")).getText();
             String text = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-qa=\"lufexuloga\"]"))).getText();
+//            String text = driver.findElement(By.cssSelector("[data-qa=\"lufexuloga\"]")).getText();
             System.out.println(text);
-            Assert.assertEquals(text,"Aman Ji");
+            Assert.assertEquals(text, "Aman Ji");
         }
         if (expectedResult.equalsIgnoreCase("InValid")) {
             WebElement error_message = driver.findElement(By.id("js-notification-box-msg"));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
             wait.until(ExpectedConditions.visibilityOf(error_message));
             Assert.assertTrue(error_message.isDisplayed());
-            Assert.assertEquals(error_message.getText(),"Your email, password, IP address or location did not match");
+            Assert.assertEquals(error_message.getText(), "Your email, password, IP address or location did not match");
         }
     }
 
 
     @DataProvider(name = "loginData")
-    public Object[][] testData() {
-    return new Object[][]{
-
-            {"TD1", "93npu2yyb0@esiix.comi", "Wingify@123", "InValid"},
-            {"TD2", "contact+atb7x@thetestingacademy.com", "Wingify@1234", "Valid"},
-
-    };
+    public String[][] testDataExcel() throws IOException {
+        String testDataFile = "src/test/java/TataData.xlsx";
+        ExcelReader excelReader = new ExcelReader(testDataFile);
+        String[][] data = excelReader.getDataFromSheet(testDataFile, "LoginData");
+        return data;
     }
-
-//    @DataProvider(name = "loginData")
-//    public String[][] testDataExcel() throws IOException {
-//        String testDataFile = "src/test/resources/TataData.xlsx";
-//        ExcelReader excelReader = new ExcelReader(testDataFile);
-//        String[][] data = excelReader.getDataFromSheet(testDataFile, "LoginData");
-//        return data;
-//    }
 
 
     @AfterSuite
